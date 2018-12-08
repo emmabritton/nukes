e7cfc76f50028a2ac3284e4c5c107014404ed7ae
Ray Britton <raybritton@gmail.com>
2018-12-08 12:19:09 +0000

    Remove analytic setup code from node js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.static(__dirname + '/public'));
 global.SERVER_HOST = process.env.HOSTNAME || 'localhost';
 global.SERVER_PORT = process.env.PORT || '3001';
 
-const file = sprintf(fs.readFileSync('./res/map.html').toString(), process.env.GOOGLE_KEY, process.env.GOOGLE_KEY);
+const file = fs.readFileSync('./res/map.html').toString();
 
 app.get('/alive', function (req, res) {
     res.sendStatus(200);
